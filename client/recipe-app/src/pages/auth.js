caa8686dbded3ca110f36874a7ad2e3a286c3c5b
Vitaly <vitaly@Vitalys-MacBook-Air.local>
2023-04-12 19:36:59 -0600

    added registration and login w/ authentication

--- client/recipe-app/src/pages/auth.js
+++ client/recipe-app/src/pages/auth.js
@@ -1,4 +1,7 @@
 import { useState } from "react"
+import axios from 'axios'
+import { useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom' 
 
 export const Auth = () => {
     return <div className = 'auth'>
@@ -10,20 +13,49 @@ export const Auth = () => {
 const Login = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    return <Form username = {username} setUsername = {setUsername} password = {password} setPassword = {setPassword} label = "Login"/>
+    const [_, setCookies] = useCookies(['access_token'])
+    const navigate = useNavigate()
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        try{
+            const response = await axios.post('http://localhost:3001/auth/login', {
+                username, 
+                password
+            })
+            setCookies('access_token', response.data.token)
+            window.localStorage.setItem('userID', response.data.userID)
+            navigate('/')
+        }catch(error){
+            console.error(error)
+        }
+    }
+    return <Form username = {username} setUsername = {setUsername} password = {password} setPassword = {setPassword} label = 'Login' onSubmit = {onSubmit}/>
 }
 
 const Register = () => { 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    return <Form username = {username} setUsername = {setUsername} password = {password} setPassword = {setPassword} label = "Register" />
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        try {
+            await axios.post('http://localhost:3001/auth/register', {
+                username,
+                password
+            })
+            alert('Registration completed. Now login.')
+        } catch(error) {
+            console.error(error)
+        }
+    }
+    return <Form username = {username} setUsername = {setUsername} password = {password} setPassword = {setPassword} label = 'Register' onSubmit = {onSubmit} />
 
 }
 
-const Form = ({username, setUsername}, {password, setPassword}, label) => {
+const Form = ({username, setUsername, password, setPassword, label, onSubmit}) => {
     return (
         <div className = 'auth-container'>
-            <form>
+            <form onSubmit = {onSubmit}>
                 <h2> {label} </h2>
                 <div className = 'form-group'>
                     <label htmlFor = 'username'> Username: </label>
